98d12f94428bc88e1c318b3c22e6c735b60fc534
remusito <rcerezomartin@gmail.com>
2025-10-12 20:06:27 +0200

    Delete ruedas.js

--- ruedas.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const wheels = document.querySelectorAll('.wheel-selector');
-
-    wheels.forEach(wheel => {
-        wheel.addEventListener('click', () => {
-            wheel.classList.toggle('selected');
-        });
-    });
-});
